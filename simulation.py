43e804fe547f5db9808af3903b001973297a5eae
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2019-05-02 08:01:03 -0400

    Updated from Brython Server: 5/2/2019 8:01:02 AM

--- simulation.py
+++ simulation.py
@@ -18,15 +18,20 @@ def draw(l):
     return choice(l)
 
 def strat_1(batch, winnings):
-    marble=draw(batch)
-    if marble == 'red':
+    if draw(batch == 'red':
         return winnings
     else:
         return 0
         
 def strat_2(batch, winnings):
-    marble=draw(batch)
-    if marble == 'green':
+    if draw(batch) == 'green':
+        return winnings
+    else:
+        return 0
+        
+def strat_3(batch, winnings):
+    guess=draw(batch)
+    if draw(batch)==guess:
         return winnings
     else:
         return 0
@@ -37,15 +42,18 @@ def run_rounds(num, batch, strat):
         winnings+=strat(batch, W)
     return ([num, winnings])
 
-def find_D(intensity):
+def find_winnings(intensity):
     earned1=0
     earned2=0
+    earned3=0
     for a in range(intensity):
         marbles = gen_marbles(N)
         earned1 += run_rounds(1, marbles, strat_1)[1]
         earned2 += run_rounds(1, marbles, strat_2)[1]
+        earned3 += run_rounds(1, marbles, strat_3)[1]
     print('''In {0} rounds: 
         Strategy 1 earned {1} per round. 
-        Strategy 2 earned {2} per round.'''.format(intensity, earned1/intensity, earned2/intensity))
+        Strategy 2 earned {2} per round.
+        Strategy 3 earned {3} per round.'''.format(intensity, earned1/intensity, earned2/intensity, earned3/intensity))
     
-find_D(500)
+#find_winnings(100000)
